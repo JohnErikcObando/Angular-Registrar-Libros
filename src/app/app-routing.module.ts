0d2d1422b508966f9530669ee4ce65cb57328b4b
JohnErikcObando <Jeobando11@hotmail.com>
2021-04-16 18:13:13 -0500

    realizacion del menu y registro login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LibrosComponent } from './pages/libros/libros.component';
+import { LoginComponent } from './seguridad/login/login.component';
+import { RegistrarComponent } from './seguridad/registrar/registrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'inicio',component: InicioComponent},
+    {path: 'libros', component: LibrosComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registrar', component: RegistrarComponent},
+    // {path: '**', pathMatch:'full', redirectTo:'inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
-import { UsuarioComponent } from './usuario.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+
 import { RegistrarComponent } from './seguridad/registrar/registrar.component';
+import { LoginComponent } from './seguridad/login/login.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LibrosComponent } from './pages/libros/libros.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuarioComponent,
     RegistrarComponent,
+    LoginComponent,
+    InicioComponent,
+    LibrosComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
